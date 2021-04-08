fc1c4b36a98f5f537aaedbb5540e86cc8eea7d1f
regcsuu <regina.toth0716@gmail.com>
2021-04-08 18:22:39 +0200

    Második Commit.

--- TothRegina_beadandó/TothRegina_beadandó/MainWindow.xaml.cs
+++ TothRegina_beadandó/TothRegina_beadandó/MainWindow.xaml.cs
@@ -20,9 +20,68 @@ namespace TothRegina_beadandó
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Szamla> szamlak = new List<Szamla>();
         public MainWindow()
         {
+            Szamla szamla1 = new Szamla(2500,"Beta");
+            Szamla szamla2 = new Szamla(3500, "Alfa");
+            szamlak.Add(szamla1);
+            szamlak.Add(szamla2);
+            //Szamla1Egyen.Text = Convert.ToString(szamlak[0].Egyenleg);
+            //Szamla2Egyen.Text = Convert.ToString(szamlak[1].Egyenleg);
+
+
             InitializeComponent();
+        }
+
+        private void Feltolt1_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[0].Egyenlegno(Convert.ToInt32(Bevitel1.Text));
+            Szamla1Egyen.Text = Convert.ToString( szamlak[0].Egyenleg);
+        }
+
+        private void Utalas1_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[0].Egyenlegcsokk(Convert.ToInt32(Bevitel1.Text));
+            szamlak[1].Egyenlegno(Convert.ToInt32(Bevitel1.Text));
+            Szamla1Egyen.Text = Convert.ToString(szamlak[0].Egyenleg);
+            Szamla2Egyen.Text = Convert.ToString(szamlak[1].Egyenleg);
+        }
+
+        private void Kivet1_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[0].Egyenlegcsokk(Convert.ToInt32(Bevitel1.Text));
+            Szamla1Egyen.Text = Convert.ToString(szamlak[0].Egyenleg);
+        }
+
+        private void NevValtas1_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[0].tulaj = Bevitel1.Text;
+            Szamla1Tul.Text = szamlak[0].tulaj;
+        }
+        private void Feltolt2_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[1].Egyenlegno(Convert.ToInt32(Bevitel2.Text));
+            Szamla2Egyen.Text = Convert.ToString(szamlak[1].Egyenleg);
+        }
+
+        private void Utalas2_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[1].Egyenlegcsokk(Convert.ToInt32(Bevitel1.Text));
+            szamlak[0].Egyenlegno(Convert.ToInt32(Bevitel1.Text));
+            Szamla1Egyen.Text = Convert.ToString(szamlak[0].Egyenleg);
+            Szamla2Egyen.Text = Convert.ToString(szamlak[1].Egyenleg);
+        }
+
+        private void Kivet2_Click(object sender, RoutedEventArgs e)
+        {
+            szamlak[1].Egyenlegcsokk(Convert.ToInt32(Bevitel2.Text));
+            Szamla2Egyen.Text = Convert.ToString(szamlak[1].Egyenleg);
+        }
+
+        private void NevValtas2_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
